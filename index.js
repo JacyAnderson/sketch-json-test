2f11eada87d86ebc3b5401127a18ebb247a14828
Jacy <jacyjamesanderson@gmail.com>
2017-05-25 11:38:15 -0600

    add function to change name text

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readFile('sketch-hello-world.sketch', function(err, data) {
 	if(err) throw err;
 
 	// Zip contains a json file that describes all the directory & files in the zip file
-	JSZip.loadAsynce(data).then(function(zip) {
+	JSZip.loadAsync(data).then(function(zip) {
 
 		// reads the top level of page
 		const pagePath = Object.keys(zip.files)[1];
@@ -18,9 +18,46 @@ fs.readFile('sketch-hello-world.sketch', function(err, data) {
 				const json = JSON.parse(str);
 
 				// grabs the first layer at index 0 (text layer)
-				const text =json.layers[0];
-				text.name = 'Changing the layer name';
+				const layerOne =json.layers[0];
 
+					// grabs the second layer at index 1
+				const layerTwo = json.layers[1];
+
+				// Layer one name
+				const vader = "Obi Wan never told you what happened to your father";
+				
+				// Layer two name
+				const luke = "He told me you killed him";
+
+
+				const isVaderLukesFather = true;
+				const likeFatherLikeSon = "NOOOOOOOOO!";
+
+				// Check for layer names
+				console.log("Before running the heToldMeEnough(), layerOne's name is: " + layerOne.name);
+				console.log("Before running the heToldMeEnough() layerTwo's name is: " + layerTwo.name);
+
+				console.log(vader);
+				// changes name for layers 
+				function heToldMeEnough() {
+					console.log("heToldMeEnough!");
+					console.log(layerTwo.name);
+
+					if(isVaderLukesFather == true) {
+						layerOne.name = "No -- I am your father";
+						console.log(layerOne.name);
+						layerTwo.name = likeFatherLikeSon;
+						console.log(layerTwo.name);
+					}	
+				}
+
+				heToldMeEnough();
+		
+
+
+				// Reassigns layer name
+				// layerOne.name = 'Hola senor';
+				// console.log(layerOne.name);
 				// write the page's json back to the zip file
 				zip.file(pagePath, JSON.stringify(json))
 
@@ -29,7 +66,7 @@ fs.readFile('sketch-hello-world.sketch', function(err, data) {
 				.generateNodeStream({ type:'nodebuffer', streamFiles:true })
 				.pipe(fs.createWriteStream('sketch-hello-world.sketch'))
 				.on('finish', () => {
-					console.log('MARIO SAVED PRINCESS SKETCH');
+					console.log('Mario saved Princess Sketch File!');
 				});
 			});
 	});
